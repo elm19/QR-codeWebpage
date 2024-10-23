6cbd1d26a40a80c56448c215dee0f47284532f92
eLM <128943656+elm19@users.noreply.github.com>
2024-10-23 20:45:23 +0100

    almost finished the design part

---
+++ README.md
@@ -0,0 +1,1 @@
+i will fill this later, OK?
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>is it working</h1>
+    
+    </div>
+  );
+}
+
+export default App;
